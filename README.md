ef6ca9bc5ce9ff7a66bd3d2ab62b6fbb8c527ba7
Yura <ybeliaev@gmail.com>
2019-03-06 15:22:49 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-easy-adding-css-class
-Created with CodeSandbox
---
+++ src/components/list/list.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ListItem from "../list-item";
+import "./list.css";
+
+const List = ({ todos }) => {
+  const elements = todos.map(item => {
+    const { id, ...itemProps } = item;
+    return (
+      <li key={id} className="list-item">
+        <ListItem {...itemProps} />
+      </li>
+    );
+  });
+  return <ul className="todo-list">{elements}</ul>;
+};
+export default List;
